a96d7ee59d1ad2ce5a8fbb24977af13f413f525e
Ilya Paddubny <ilyapaddubny@gmail.com>
2023-09-27 15:32:58 +0300

    Initial Commit

---
+++ Set! Card game/Set__Card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set__Card_gameApp.swift
+//  Set! Card game
+//
+//  Created by Ilya Paddubny on 27.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Set__Card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
